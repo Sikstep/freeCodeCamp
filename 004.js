b12db60a456216f2302dbbc7b37fcb5d47255eb7
Sikstep <Sikstep1988@gmail.com>
2023-01-18 12:02:49 +0300

    40%

---
+++ 004.js
@@ -0,0 +1,18 @@
+"use strict";
+// // Setup
+// const myArray = [
+//   ["John", 23],
+//   ["cat", 2],
+// ];
+
+// Only change code below this line
+myArray.push(["dog", 3]);
+
+// Setup
+const myArray = [
+  ["John", 23],
+  ["cat", 2],
+];
+
+// Only change code below this line
+const removedFromMyArray = myArray.pop();
