2508835daeb9a4512a95abe93a62713b0cb8e902
Sikstep <Sikstep1988@gmail.com>
2023-01-18 11:35:33 +0300

    32%

--- 003.js
+++ 003.js
@@ -1,7 +1,19 @@
-'use strict';
-let a = 'Asome flash\r dasvv and \t lolz \\ kik';
+"use strict";
+let a = "Asome flash\r dasvv and \t lolz \\ kik";
 console.log(a);
 
 const myStr = "FirstLine\n\t\t\\SecondLine\nThirdLine";
 console.log(myStr);
 
+// Setup
+// const lastName = "Lovelace";
+
+// Only change code below this line
+const thirdLetterOfLastName = lastName[2]; // Change this line
+
+// Setup
+const lastName = "Lovelace";
+
+// Only change code below this line
+const lastLetterOfLastName = lastName[lastName.length - 1]; // Change this line
+console.log(lastLetterOfLastName);
