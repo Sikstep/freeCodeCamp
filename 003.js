bb1f44f2c3c3cf26c59a590d0527624b5a3cd2f1
Sikstep <Sikstep1988@gmail.com>
2023-01-18 11:55:16 +0300

    38%

--- 003.js
+++ 003.js
@@ -40,10 +40,19 @@ console.log(wordBlanks);
 // Only change code below this line
 // const myArray = [['myName', 'Kosty'], [7, 9]];
 // const myArray = [50, 60, 70];
-const myData = myArray[0];
+// const myData = myArray[0];
 
 // Setup
-const myArray = [18, 64, 99];
+// const myArray = [18, 64, 99];
 
 // Only change code below this line
-myArray[0] = 45;
+// myArray[0] = 45;
+
+const myArray = [
+  [1, 2, 3],
+  [4, 5, 6],
+  [7, 8, 9],
+  [[10, 11, 12], 13, 14],
+];
+
+const myData = myArray[2][1];
