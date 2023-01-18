35c5f09558f8281908335d36078bc7e85f72449a
Sikstep <Sikstep1988@gmail.com>
2023-01-18 11:43:36 +0300

    34%

--- 003.js
+++ 003.js
@@ -23,3 +23,14 @@ const lastName = "Lovelace";
 
 // Only change code below this line
 const secondToLastLetterOfLastName = lastName[lastName.length - 2]; // Change this line
+
+const myNoun = "dog";
+const myAdjective = "big";
+const myVerb = "ran";
+const myAdverb = "quickly";
+
+// Only change code below this line
+const wordBlanks =
+  "A " + myAdjective + " " + myNoun + " " + myVerb + " " + myAdverb + "."; // Change this line
+// Only change code above this line
+console.log(wordBlanks);
