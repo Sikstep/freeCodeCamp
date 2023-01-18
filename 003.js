8d70a775c680c652b3023459ebbd36678e0ee672
Sikstep <Sikstep1988@gmail.com>
2023-01-18 11:37:15 +0300

    33%

--- 003.js
+++ 003.js
@@ -12,8 +12,14 @@ console.log(myStr);
 const thirdLetterOfLastName = lastName[2]; // Change this line
 
 // Setup
-const lastName = "Lovelace";
+// const lastName = "Lovelace";
 
 // Only change code below this line
 const lastLetterOfLastName = lastName[lastName.length - 1]; // Change this line
 console.log(lastLetterOfLastName);
+
+// Setup
+const lastName = "Lovelace";
+
+// Only change code below this line
+const secondToLastLetterOfLastName = lastName[lastName.length - 2]; // Change this line
