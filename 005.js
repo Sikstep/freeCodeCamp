19c29fc5db14c167911709618ca5ac47ba4aac34
Sikstep <Sikstep1988@gmail.com>
2023-01-18 12:11:25 +0300

    42%

---
+++ 005.js
@@ -0,0 +1,29 @@
+"use strict";
+// Setup
+// const myArray = [["John", 23], ["dog", 3]];
+
+// Only change code below this line
+const removedFromMyArray = myArray.shift();
+
+// Setup
+const myArray = [
+  ["John", 23],
+  ["dog", 3],
+];
+myArray.shift();
+
+// Only change code below this line
+myArray.unshift(["Paul", 35]);
+
+const myList = [
+  ["choco", 5],
+  ["koko", 3],
+  ["bobo", 10],
+  ["sosiges", 98],
+  ["coffee", 15],
+];
+
+function reusableFunction() {
+  console.log("Hi World");
+}
+reusableFunction();
