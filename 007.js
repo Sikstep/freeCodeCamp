1bafb47bbbf527971f9f7a2b981433fac780726a
Sikstep <Sikstep1988@gmail.com>
2023-01-19 11:01:12 +0300

    68%

--- 007.js
+++ 007.js
@@ -27,3 +27,36 @@ function testSize(num) {
 }
 
 testSize(7);
+
+const names = [
+  "Hole-in-one!",
+  "Eagle",
+  "Birdie",
+  "Par",
+  "Bogey",
+  "Double Bogey",
+  "Go Home!",
+];
+
+function golfScore(par, strokes) {
+  // Only change code below this line
+  if (strokes == 1) {
+    return names[0];
+  } else if (par - strokes == 0) {
+    return names[3];
+  } else if (par - strokes <= -3) {
+    return names[6];
+  } else if (par - strokes <= -2) {
+    return names[5];
+  } else if (par - strokes <= -1) {
+    return names[4];
+  }
+  // Only change code above this line
+  else if (par - strokes >= 2) {
+    return names[1];
+  } else if (par - strokes >= 1) {
+    return names[2];
+  }
+}
+
+console.log(golfScore(5, 5));
