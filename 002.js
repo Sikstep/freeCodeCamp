7cc2389aa089119eea8e278b7e937f204c7de624
Sikstep <Sikstep1988@gmail.com>
2023-01-20 14:47:32 +0300

    75%

--- 002.js
+++ 002.js
@@ -170,3 +170,14 @@ const testObj = {
 // Only change code below this line
 const hatValue = testObj.hat; // Change this line
 const shirtValue = testObj.shirt;
+
+// Setup
+const testObj = {
+  "an entree": "hamburger",
+  "my side": "veggies",
+  "the drink": "water",
+};
+
+// Only change code below this line
+const entreeValue = testObj["an entree"]; // Change this line
+const drinkValue = testObj["the drink"]; // Change this line
